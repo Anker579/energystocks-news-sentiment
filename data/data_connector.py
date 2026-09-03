b4497380e7ef769e3e33a49d6c49808ffec9fe2b
Anker579 <angushoward03@gmail.com>
2026-09-03 15:13:33 +0100

    added .env and begun api fetchers

---
+++ data/data_connector.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import pandas as pd
+import requests
+import os
+
+class news_api_connector():
+    def __init__(self) -> None:
+        None
+
+    def get_newsdataio(self, token,):
+        pass
+
+    def get_marketaux(self, token,):
+        pass
+
+    def get_gdelt(self, token,):
+        pass
+
+
+
+
+
