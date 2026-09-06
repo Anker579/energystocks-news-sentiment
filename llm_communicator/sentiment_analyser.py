1a0159d6a646cb5071622f715b8284a9e964bbad
Angus Howard <angushoward@Anguss-MacBook-Pro.local>
2026-09-06 19:29:27 +0100

    llm can analyse and return sentiment after 330 seconds

--- llm_communicator/sentiment_analyser.py
+++ llm_communicator/sentiment_analyser.py
@@ -1,23 +1,48 @@
 import requests
+import json
+from pathlib import Path
 
-def analyse_sentiment(article: str, title: str) -> dict:
+
+INPUT_DIR = Path(__file__).parent / "llm_inputs"
+
+
+def analyse_sentiment(article: str, title: str) -> str:
+
+    with open(INPUT_DIR / "monitored_stocks.json", "r") as f:
+        monitored_stocks = json.load(f)
+
+    allowed_stocks = json.dumps(monitored_stocks)
+
+    with open(INPUT_DIR / "system_input.txt", "r") as f:
+        system_prompt = f.read()
+
     response = requests.post(
-       "http://localhost:8080/v1/chat/completions",
-       json={
-           "model": "local-model",
-           "messages": [
-               {
-                   "role": "system",
-                   "content": "You are a stock market news sentiment analysis assistant. You will be provided with fulltext news articles and you will provide for each article: a boolean of whether the article matches the title provided, the relevant stock symbols or companies, a sentiment score for each and your confidence level on a scale of 0 to 1 on the sentiment score for each. The sentiment score should be a number between -1 and 1, where -1 indicates very negative sentiment, 0 indicates neutral sentiment, and 1 indicates very positive sentiment. Your answer should be returned as a python dictionary. If the article does not match the title, return a sentiment score of 0 and an empty list of stock symbols or companies."
-               },
-               {
-                   "role": "user",
-                   "content": f"The fulltext article: {article}, the title: {title}."
-               }
-           ],
-           "temperature": 0
-       }
+        "http://localhost:8080/v1/chat/completions",
+        json={
+            "model": "local-model",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": system_prompt
+                },
+                {
+                    "role": "user",
+                    "content": f"""
+Allowed stocks:
+{allowed_stocks}
+
+Title:
+{title}
+
+Article:
+{article}
+"""
+                }
+            ],
+            "temperature": 0
+        }
     )
+
     r = response.json()["choices"][0]["message"]["content"]
-    #print(r)
+
     return r
--- test_fetchers.py
+++ test_fetchers.py
@@ -1,6 +1,9 @@
 from data.data_connector import NewsAPIConnector
-import json
 from data.fulltext_retriever import get_fulltext
+from llm_communicator.sentiment_analyser import analyse_sentiment
+import json
+import time
+import subprocess
 
 connector = NewsAPIConnector()
 
@@ -10,7 +13,7 @@ m_data = connector.get_marketaux(
     )[["title", "publisher", "matched_symbols","url", "provider_sentiment", "image_url",]]
 
 page_ft = get_fulltext(m_data["url"].iloc[0])
-print(page_ft)
+#print(page_ft)
 
 print("\nNEWSDATA")
 IO_data = connector.get_newsdataio(
@@ -31,3 +34,30 @@ with open("marketaux.json", "w") as f:
 #        '"nuclear energy"'
 #    )[["title", "publisher"]]
 #)
+
+with open("page_ft.txt", "w") as f:
+    f.write(page_ft)
+
+start = time.perf_counter()
+
+analysed_article = analyse_sentiment(
+    article=page_ft,
+    title=m_data["title"].iloc[0]
+)
+
+analysed_article = json.loads(analysed_article)
+
+end = time.perf_counter()
+
+elapsed = end - start
+
+print(f"Time taken: {elapsed:.3f} seconds")
+
+with open("analysed_article.json", "w") as f:
+    json.dump(analysed_article, f, indent=4
+    )
+
+subprocess.run([
+    "afplay",
+    "/System/Library/Sounds/Glass.aiff"
+])
